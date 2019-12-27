31ce3af5a6e3b236e549275e7b4f1b800967bf3e
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2019-12-26 21:16:23 -0500

    Update async.js

--- async.js
+++ async.js
@@ -16,6 +16,7 @@ function testMe() {
 
 function delayedGreet() {
   // ADD CODE HERE
+  setTimeout(() => console.log('welcome'), 3000);
 }
 // Uncomment the following line to check your work!
 // delayedGreet(); // should log (after 3 seconds): welcome
